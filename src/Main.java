029a50708ed60c992699849cbf67eaab0acb5637
Antonio <antromer@gmail.com>
2024-04-11 21:24:14 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Nuevo cambio");
+        System.out.println("Segundo cambio");
+    }
+}
